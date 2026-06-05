a2ac5f14c7af402952e5ec36bb1bbd8a4ed19fc6
roman_vlasiuk <vlasiukrv@gmail.com>
2026-06-04 23:34:47 -0500

    tsb-enterprise - use common asto config

--- projects/tsb-enterprise/astro.config.mjs
+++ projects/tsb-enterprise/astro.config.mjs
@@ -1,65 +1,26 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+// projects/tsb-enterprise/astro.config.mjs
 
-const project_dir = '/tsb-enterprise'
+import { defineConfig } from 'astro/config';
+import { getBaseConfig } from '../base-astro.config.mjs';
+
+const baseConfig = getBaseConfig('/tsb-enterprise', 'https://tsb-enterprise.com/');
 
 export default defineConfig({
-    site: 'https://tsb-enterprise.com/',
+    ...baseConfig,
 
-    base: '/',
-    srcDir: './src',
-    outDir: '../../storage'+project_dir,
-
-    output: 'static', //'static' 'hybrid' оставит работающими API-эндпоинты
-    integrations: [
-        vue(),
-        sitemap(),
-    ],
-    markdown: {
-        shikiConfig: { theme: 'dracula' },
-    },
-    build: {
-        inlineStylesheets: 'always',
-        cacheDir: '../../node_modules/.cache/astro',
-    },
     vite: {
-        cacheDir: '../../node_modules/.cache/vite',
-        plugins: [tailwindcss()],
+        ...baseConfig.vite,
         server: {
             proxy: {
                 '/api': {
                     target: 'http://host.docker.internal:8000',
                     changeOrigin: true,
                     secure: false,
-
                     rewrite: (path) => path,
-                    headers: {
-                        'X-Client-ID': 'client_tsb'
-                    },
-                    configure: (proxy, options) => {
-                        proxy.on('error', (err, req, res) => {
-                            console.log('--- PROXY ERROR ---', err);
-                        });
-                        proxy.on('proxyReq', (proxyReq, req, res) => {
-                            console.log('--> Sending Request to Synapse:', req.method, req.url);
-                        });
-                        proxy.on('proxyRes', (proxyRes, req, res) => {
-                            console.log('<-- Received Response from Synapse:', proxyRes.statusCode);
-                        });
-                    }
+                    headers: { 'X-Client-ID': 'client_tsb' }
                 }
             }
         }
-    },
-    image: {
-        domains: [],
-        service: {
-            entrypoint: 'astro/assets/services/sharp',
-        },
-    },
-    devToolbar: {
-        enabled: false
     }
+
 });
