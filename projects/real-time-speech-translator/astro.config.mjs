47c9db642e4b7f3ad7f194c18ad66a2cf5aedbd9
roman_vlasiuk <vlasiukrv@gmail.com>
2026-06-04 23:48:52 -0500

    real-time-speech-translator - use common astro config - Edited main layout content

--- projects/real-time-speech-translator/astro.config.mjs
+++ projects/real-time-speech-translator/astro.config.mjs
@@ -1,40 +1,11 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+// projects/real-time-speech-translator/astro.config.mjs
 
-const project_dir = '/real-time-speech-translator'
+import { defineConfig } from 'astro/config';
+import { getBaseConfig } from '../base-astro.config.mjs';
+
+const project_dir = '/real-time-speech-translator';
+const baseConfig = getBaseConfig(project_dir, 'https://speech-translator.tsb-enterprise.com');
 
 export default defineConfig({
-    site: 'https://speech-translator.tsb-enterprise.com/',
-
-    base: '/',
-    srcDir: './src',
-    outDir: '../../storage'+project_dir,
-
-    output: 'static', //'static' 'hybrid' оставит работающими API-эндпоинты
-    integrations: [
-        vue(),
-        sitemap(),
-    ],
-    markdown: {
-        shikiConfig: { theme: 'dracula' },
-    },
-    build: {
-        inlineStylesheets: 'always',
-        cacheDir: '../../node_modules/.cache/astro',
-    },
-    vite: {
-        plugins: [tailwindcss()],
-        cacheDir: '../../node_modules/.cache/vite',
-    },
-    image: {
-        domains: [],
-        service: {
-            entrypoint: 'astro/assets/services/sharp',
-        },
-    },
-    devToolbar: {
-        enabled: false
-    }
+    ...baseConfig
 });
