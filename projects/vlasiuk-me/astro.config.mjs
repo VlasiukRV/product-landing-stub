14169e7a96a8e1cffdb3d20610979faa85437a05
roman_vlasiuk <vlasiukrv@gmail.com>
2026-06-05 18:29:50 -0500

    vlasiuk-me
    - use base astro config
    - Edited content

--- projects/vlasiuk-me/astro.config.mjs
+++ projects/vlasiuk-me/astro.config.mjs
@@ -1,40 +1,11 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+// projects/vlasiuk-me/astro.config.mjs
 
-const project_dir = '/vlasiuk-me'
+import { defineConfig } from 'astro/config';
+import { getBaseConfig } from '../base-astro.config.mjs';
+
+const project_dir = '/vlasiuk-me';
+const baseConfig = getBaseConfig(project_dir, 'https://vlasiuk.me');
 
 export default defineConfig({
-    site: 'https://vlasiuk.me/',
-
-    base: '/',
-    srcDir: './src',
-    outDir: '../../storage'+project_dir,
-
-    output: 'static', //'static' 'hybrid' оставит работающими API-эндпоинты
-    integrations: [
-        vue(),
-        sitemap(),
-    ],
-    markdown: {
-        shikiConfig: { theme: 'dracula' },
-    },
-    build: {
-        inlineStylesheets: 'always',
-        cacheDir: '../../node_modules/.cache/astro',
-    },
-    vite: {
-        plugins: [tailwindcss()],
-        cacheDir: '../../node_modules/.cache/vite',
-    },
-    image: {
-        domains: [],
-        service: {
-            entrypoint: 'astro/assets/services/sharp',
-        },
-    },
-    devToolbar: {
-        enabled: false
-    }
+    ...baseConfig
 });
